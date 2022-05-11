29d7fd96e83f05265c3d79f4454e2791d86945a5
KojoZero <kwaku.kiriguya@gmail.com>
2022-05-10 19:30:35 -0700

    Initialized

--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,10 @@
+import os
 import re
 import json
 import time
 import requests
 
-def getMangaData(mangaLink):
+def getMangaData(mangaLink, ref=None):
     res = requests.get(mangaLink)
     html = res.text
     data = {}
@@ -15,7 +16,11 @@ def getMangaData(mangaLink):
     title = getDataFromExpression(r'<div class="post-title">[\s\S]+<h1>\s+([^<]+)')
     summary = getDataFromExpression(r'<div class="summary__content ">[\s\S]+?<p>([\s\S]+?)<\/p>')
     cover = getDataFromExpression(r'<div class="summary_image">[\s\S]+?src="([^"]+)')
-    author = getDataFromExpression(r'<div class="author-content">[\s\S]+?>([^<]+)')
+    
+    try:
+        author = getDataFromExpression(r'<div class="author-content">[\s\S]+?>([^<]+)')
+    except:
+        author = ""
 
     data['title'] = title
     data['volume'] = 1
@@ -29,45 +34,78 @@ def getMangaData(mangaLink):
     chapters = re.findall(chapterExpr, html)
 
     for chapterLink in chapters:
-        res = requests.get(chapterLink)
-        html = res.text
         chapterData = {}
         
         chapterIndexExpr = r'(\d+)[^\/]*\/$'
         match = re.search(chapterIndexExpr, chapterLink)
         chapterIndex = match.group(1)
         
-        chapterHeaderExpr = r'id="chapter-heading">([^<]+)</h'
-        match = re.search(chapterHeaderExpr, html)
-        chapterHeader = match.group(1)
+        if ref:
+            chapterData = ref['chapters'].get(chapterIndex) or {}
 
-        chapterNameExpr = r'[\s\S]+-(.+)'
-        match = re.search(chapterNameExpr, chapterHeader)
-        chapterName = match.group(1).strip()
+        if not chapterData:
+            res = requests.get(chapterLink)
+            html = res.text
+            
+            chapterHeaderExpr = r'id="chapter-heading">([^<]+)</h'
+            match = re.search(chapterHeaderExpr, html)
+            chapterHeader = match.group(1)
 
-        imageDataExpr = r'<img id="image-(\d+)" src="[^h]+([^"]+)'
-        imageMatches = re.findall(imageDataExpr, html)
-        images = []
+            chapterNameExpr = r'[\s\S]+-(.+)'
+            match = re.search(chapterNameExpr, chapterHeader)
+            chapterName = match.group(1).strip()
 
-        for imgMatch in imageMatches:
-            index = int(imgMatch[0])
-            imgLink = imgMatch[1]
-            images.insert(index, imgLink)
+            imageDataExpr = r'<img id="image-(\d+)" src="[^h]+([^"]+)'
+            imageMatches = re.findall(imageDataExpr, html)
+            images = []
 
-        chapterData["title"] = chapterName
-        chapterData["groups"] = {}
-        chapterData["groups"]["Colored Council"] = images
+            for imgMatch in imageMatches:
+                index = int(imgMatch[0])
+                imgLink = imgMatch[1]
+                images.insert(index, imgLink)
+
+            chapterData["title"] = chapterName
+            chapterData["groups"] = {}
+            chapterData["groups"]["Colored Council"] = images
+        
         chapterData['last_updated'] = str(int(time.time())) # TODO: scrape time
         data['chapters'][chapterIndex] = chapterData
 
     return data
 
 if __name__ == "__main__":   
-    mangaData = getMangaData("https://coloredmanga.com/manga/kingdom/")
+    res = requests.get("https://coloredmanga.com/")
+    html = res.text
+    seriesExpr = r'<h3[^>]+>[^<]*<a\s+href="(https:\/\/coloredmanga.com\/manga\/[^"\/]+)["\/]'
+    seriesMatches = set(re.findall(seriesExpr, html))
 
-    with open("./json/kingdom_metadata.json", "w") as file:
-        json.dump(mangaData, file, indent=4)
+    for series in seriesMatches:
+        print("LOADING MANGA DATA FOR", series)
 
-# https://cubari.moe/gist/https://raw.githubusercontent.com/KojoZero/CubariIntegration/master/json/kingdom_metadata.json
-# regex for series info
-# r'href="(https:\/\/coloredmanga.com\/manga\/[^"\/]+)["\/]'
+        seriesNameExpr = r'.+\/(.+)'
+        match = re.search(seriesNameExpr, series)
+        seriesName = match.group(1)
+
+        ref = None
+        outputPath = "./json/" + seriesName + ".json"
+
+        if os.path.exists(outputPath):
+            with open(outputPath) as file:
+                ref = json.load(file)
+
+        try:
+            mangaData = getMangaData(series, ref)
+            print("LOADED MANGA DATA FOR", series)
+
+            with open(outputPath, "w") as file:
+                json.dump(mangaData, file, indent=4)
+                
+            print("EXPORTED TO", outputPath)
+        except Exception as e:
+            print("FAILED TO LOAD MANGA FOR", series)
+            print("EXCEPTION:", e)
+            print()
+
+        
+        
+    # https://cubari.moe/gist/https://raw.githubusercontent.com/KojoZero/CubariIntegration/master/json/kingdom_metadata.json
